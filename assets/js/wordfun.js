2384ac19f068f78a7b37f519f41d0923d8c2acec
Andy Stabler <andy_60@hotmail.co.uk>
2015-06-30 00:13:15 +0100

    UPDATE: correct explosion behaviour from pellets

--- assets/js/wordfun.js
+++ assets/js/wordfun.js
@@ -1,14 +1,7 @@
-/**
- * Created by andy on 27/06/15.
- */
-
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
 // requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-
 // MIT license
-
 (function () {
     var lastTime = 0;
     var vendors = ['ms', 'moz', 'webkit', 'o'];
@@ -36,19 +29,185 @@
         };
 }());
 
-document.getElementById('word-fun-input').addEventListener('keypress', temp);
-document.getElementById('word-fun-input').focus();
+function Animator(canvas) {
+    "use strict";
+    canvas.setAttribute('width', '500');
+    canvas.setAttribute('height', '500');
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.ctx = canvas.getContext('2d');
+    this.exploders = [];
+    this.clearCanvas();
+}
 
-function temp() {
-    var anim = new Animator(document.getElementById('word-fun-canvas'));
-    var pellets = anim.createPellets(200, 200);
-    var request = requestAnimationFrame(function () {
-        anim.explode(pellets)
+Animator.prototype.pushExploder = function (exploder) {
+    "use strict";
+    var that = this;
+    // add the exploder to the beginning of the
+    this.exploders.push(exploder);
+
+    if (!running) {
+        running = true;
+        requestAnimationFrame(anim.explode.bind(anim));
+    }
+    // remove the exploder after some time
+    setTimeout(function () {
+        that.shiftExploder();
+    }, 1000);
+};
+
+Animator.prototype.shiftExploder = function () {
+    "use strict";
+    // remove from beginning
+    this.exploders.shift();
+    // stop the animation if there are no explosions to animte
+    if (this.exploders.length == 0) {
+        cancelAnimationFrame(animRequest);
+        running = false;
+        this.clearCanvas();
+    }
+};
+
+Animator.prototype.clearCanvas = function () {
+    "use strict";
+    //this.ctx.clearRect(0, 0, this.width, this.height);
+    this.ctx.fillStyle = '#000000';
+    this.ctx.fillRect(0, 0, this.width, this.height);
+};
+
+Animator.prototype.explode = function () {
+    "use strict";
+    this.clearCanvas();
+    this.exploders.forEach(function (exploder) {
+        exploder.explode();
     });
 
-    setTimeout(function () {
-        cancelAnimationFrame(request)
-    }, 5000);
+    animRequest = requestAnimationFrame(this.explode.bind(this));
+};
+
+document.getElementById('word-fun-input').addEventListener('keypress', newCharacter);
+document.getElementById('pause').addEventListener('click',
+    function () {
+        if (running) {
+            cancelAnimationFrame(animRequest);
+            running = false;
+        } else {
+            requestAnimationFrame(anim.explode.bind(anim));
+            running = true;
+        }
+    });
+
+function Exploder(ctx, x, y) {
+    "use strict";
+    this.ctx = ctx;
+    this.makePellets(x, y, 10);
+}
+
+Exploder.prototype.makePellets = function (x, y, count) {
+    "use strict";
+    var pellets = [];
+    for (var i = 0; i < count; i++)
+        pellets.push(new Pellet(x, y));
+    this.pellets = pellets;
+};
+
+Exploder.prototype.explode = function () {
+    "use strict";
+    this.draw();
+    // increase the angle of rotation
+    this.pellets.forEach(function (pellet) {
+        pellet.angle += 3 * Math.PI / 180;
+        pellet.updatePosition();
+    });
+};
+
+Exploder.prototype.draw = function () {
+    "use strict";
+    var that = this;
+    this.pellets.forEach(function (pellet) {
+        pellet.draw(that.ctx);
+    });
+
+    // just the center dot for debugging
+    this.ctx.fillStyle = "#cccccc";
+    this.ctx.fillRect(200, 200, 5, 5);
+};
+
+function Pellet(x, y) {
+    this.size = 25;
+    this.x = x;
+    this.y = y;
+    this.opacity = 1;
+    this.generateArcCx();
+    this.generateArcCy();
+    // radius is the hyp
+    this.arcRadius = Math.sqrt(Math.pow(Math.abs(this.arcCx - this.x), 2) + Math.pow(Math.abs(this.arcCy - this.y), 2));
+    this.colour = randomColor();
+    this.angle = 3 * Math.PI / 180;
+    console.log("arcCx = " + this.arcCx + ". this.x = " + this.x + ". is left arc? " + (this.arcCx < this.x) + ". Colour is " + this.colour);
+}
+
+Pellet.prototype.generateArcCx = function () {
+    // decide between a left and a right arc at random
+    this.isLeftArc = Math.random() < 0.5;
+    if (this.isLeftArc) {
+        // a left arc has been chosen
+        var max = this.x - this.size;
+        var min = this.x - 4 * this.size;//4 * this.size;
+    } else {
+        // a right arc has been chosen
+        max = this.x + 5 * this.size;
+        min = this.x + 2 * this.size;
+    }
+    this.arcCx = Math.floor(Math.random() * (max - min)) + min;
+};
+
+Pellet.prototype.generateArcCy = function () {
+    var max = this.y;// + this.size / 2; // todo: maybe make max this.y?
+    var min = this.y - this.size;
+    this.arcCy = Math.floor(Math.random() * (max - min)) + min;
+};
+
+Pellet.prototype.updatePosition = function () {
+    // check if we're firing to the left
+    // update coordinates
+
+    var xRad = this.isLeftArc ? this.arcRadius : -this.arcRadius;
+
+    this.x = this.arcCx + xRad * Math.cos(this.angle);//(this.isLeftArc ? Math.sin(this.angle) : Math.cos(this.angle));
+    this.y = this.arcCy - this.arcRadius * Math.sin(this.angle);//(this.isLeftArc ? Math.cos(this.angle) : Math.sin(this.angle));
+    //this.opacity += 0.001;
+};
+
+Pellet.prototype.draw = function (ctx) {
+    ctx.save();
+    ctx.globalAlpha = this.opacity;
+    ctx.fillStyle = this.colour;
+
+    ctx.beginPath();
+    //ctx.rotate(this.angle);
+    ctx.rect(this.x, this.y, this.size, this.size);
+
+    ctx.fill();
+    ctx.restore();
+};
+
+function randomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++)
+        color += letters[Math.floor(Math.random() * 16)];
+    return color;
+}
+
+
+document.getElementById('word-fun-input').focus();
+var anim = new Animator(document.getElementById('word-fun-canvas'));
+var running = false;
+var animRequest;
+function newCharacter() {
+    "use strict";
+    anim.pushExploder(new Exploder(anim.ctx, 200, 200));
 }
 
 /*
@@ -77,101 +236,4 @@ function update(evt) {
     // integer range = 48-57 (min/max inclusive)
     // char range = 65-90, 97-122
 
-}
-
-function Animator(canvas) {
-    canvas.setAttribute('width', '500');
-    canvas.setAttribute('height', '500');
-    this.width = canvas.width;
-    this.height = canvas.height;
-    this.ctx = canvas.getContext('2d');
-}
-
-Animator.prototype.createPellets = function (x, y) {
-    return makePellets(x, y, 5);
-};
-
-Animator.prototype.explode = function (pellets) {
-    this.draw(pellets);
-    // increase the angle of rotation
-    pellets.forEach(function (pellet) {
-        pellet.angle += 3 * Math.PI / 180;
-        pellet.updatePosition();
-    });
-    requestAnimationFrame(function () {
-        this.explode(pellets)
-    }.bind(this));
-};
-
-Animator.prototype.draw = function (pellets) {
-    //this.ctx.clearRect(0, 0, this.width, this.height);
-    var that = this;
-    pellets.forEach(function (pellet) {
-        pellet.draw(that.ctx);
-    });
-    this.ctx.fillStyle = "#000000";
-    this.ctx.fillRect(200, 200, 25, 25);
-};
-
-function makePellets(x, y, count) {
-    var pellets = [];
-    for (var i = 0; i < count; i++)
-        pellets.push(new Pellet(x, y));
-    return pellets;
-}
-
-function Pellet(x, y) {
-    this.size = 25;
-    this.x = x;//Math.floor(Math.random() * (x + 100 - x - 100)) + x - 100;//
-    this.y = y;//Math.floor(Math.random() * (y + 100 - y - 100)) + y - 100;//
-    this.generateArcCx();
-    this.generateArcCy();
-    // radius is the hyp
-    this.arcRadius = Math.sqrt(Math.pow(this.arcCx, 2) + Math.pow(this.arcCy, 2));
-    this.colour = randomColor();
-    this.angle = 3 * Math.PI / 180;
-    console.log("arcCx = " + this.arcCx + ". this.x = " + this.x + ". is left arc? " + (this.arcCx < this.x) + ". Colour is " + this.colour);
-}
-
-Pellet.prototype.generateArcCx = function () {
-    // decide between a left and a right arc at random
-    this.isLeftArc = Math.random() < 0.5;
-    if (this.isLeftArc) {
-        // a left arc has been chosen
-        var max = this.x - this.size; //500;
-        var min = this.x - 4 * this.size; //0;//
-    } else {
-        // a right arc has been chosen
-        max = this.x + 5 * this.size; //500;//
-        min = this.x + 2 * this.size; //0;//
-    }
-    this.arcCx = Math.floor(Math.random() * (max - min)) + min;
-};
-
-Pellet.prototype.generateArcCy = function () {
-    var max = this.y + this.size / 2;
-    var min = this.y - this.size;
-    this.arcCy = Math.floor(Math.random() * (max - min)) + min;
-};
-
-Pellet.prototype.updatePosition = function () {
-    // check if we're firing to the left
-    // update coordinates
-    this.x = this.arcCx + this.size * (this.isLeftArc ? Math.sin(this.angle) : Math.cos(this.angle));
-    this.y = this.arcCy + this.size * (this.isLeftArc ? Math.cos(this.angle) : Math.sin(this.angle))*2;
-};
-
-Pellet.prototype.draw = function (ctx) {
-    ctx.fillStyle = this.colour;
-    ctx.beginPath();
-    ctx.rect(this.x, this.y, this.size, this.size);
-    ctx.fill();
-};
-
-function randomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++)
-        color += letters[Math.floor(Math.random() * 16)];
-    return color;
 }

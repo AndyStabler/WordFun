c48e7370fc0901a5d461520d1d9b67bcf93fee1e
Andy Stabler <andy_60@hotmail.co.uk>
2015-07-06 23:17:16 +0100

    UPDATE: display

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,29 @@
  * Created by Andy Stabler
  */
 var anim = new wordFun.animator();
+var displayTimer;
+var displayRunning = false;
 
 document.getElementById('word-fun-input').addEventListener('keypress', anim.fire.bind(anim));
-document.getElementById('word-fun-fire').addEventListener('click', anim.fire.bind(anim));
 document.getElementById('word-fun-input').focus();
+document.getElementById('word-fun-auto').addEventListener('click',
+    function () {
+        "use strict";
+        var autoButton = document.getElementById('word-fun-auto');
+        if (!displayRunning) {
+            displayRunning = !displayRunning;
+            autoButton.innerHTML = "Stop Display";
+            displayTimer = requestAnimationFrame(startDisplay);
+        } else {
+            displayRunning = !displayRunning;
+            autoButton.innerHTML = "Start Display";
+            cancelAnimationFrame(displayTimer);
+        }
+    });
+
+
+function startDisplay() {
+    "use strict";
+    anim.fire.bind(anim)();
+    displayTimer = requestAnimationFrame(startDisplay);
+}
